ee0003759f3a1e1dc14ad8aeeec04dd4330d8f64
VitaliyVovk76 <vovkvitol@gmail.com>
2023-04-25 17:52:59 +0300

    ytp bpv

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './componenrts/Layout';
-import HomePage from './pages/HomePage';
+import About from './pages/About';
 import ContactPage from './pages/ContactsPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />}></Route>
+        <Route index element={<About />}></Route>
         <Route path="contacts" element={<ContactPage />}></Route>
       </Route>
     </Routes>
